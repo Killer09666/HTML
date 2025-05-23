32e1d0b8e75de602ae319e167cef32a9942d11f1
Killer09666 <nabajitmuhury43@gmail.com>
2025-05-23 23:03:36 +0600

    java scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = 56;
+var b = 29;
+
+document.getElementById('n1').innerHTML = a;
+document.getElementById('n2').innerHTML = b;
+
+document.getElementById('add').innerHTML = a + b;
+document.getElementById('sub').innerHTML = a - b;
+document.getElementById('multiply').innerHTML = a * b;
+document.getElementById('divide').innerHTML = a / b;
