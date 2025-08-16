c651d0ebab47e46f67ee6ca9f29238914aa36438
Killer09666 <nabajitmuhury43@gmail.com>
2025-08-16 22:50:31 +0600

    new

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var a = 56;
-var b = 29;
-
-document.getElementById('n1').innerHTML = a;
-document.getElementById('n2').innerHTML = b;
-
-document.getElementById('add').innerHTML = a + b;
-document.getElementById('sub').innerHTML = a - b;
-document.getElementById('multiply').innerHTML = a * b;
-document.getElementById('divide').innerHTML = a / b;
+function checkPalindrome(){
+    const input = document.getElementById("textInput").ariaValueMax;
+    const result = document.getElementById("result");
+    const cleanedInput = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversed = cleanedInput.split('').reverse().join('');
+    if(cleanedInput.length === 0) {
+        result.innerHTML = "Please enter a valid string.";
+    }
+    else if (cleanedInput === reversed) {
+        result.innerHTML = `"${input}" is a palindrome.`;
+        result.style.color = "green";
+    } else {
+        result.innerHTML = `"${input}" is not a palindrome.`;
+        result.style.collor = "red";
+    }
+}
