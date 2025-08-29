7929f81220fe7e7ba118b6b3fd072e78c5262fb9
Killer09666 <nabajitmuhury43@gmail.com>
2025-08-29 22:57:01 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-function checkPalindrome(){
-    const input = document.getElementById("textInput").ariaValueMax;
-    const result = document.getElementById("result");
-    const cleanedInput = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    const reversed = cleanedInput.split('').reverse().join('');
-    if(cleanedInput.length === 0) {
-        result.innerHTML = "Please enter a valid string.";
+document.addEventListener("DOMContentLoaded", ()=>{
+    const timeDisplay = document.getElementById("timeDisplay");
+    const startBtn = document.getElementById("startBtn");
+    const pauseBtn = document.getElementById("pauseBtn");
+    const resetBtn = document.getElementById("resetBtn");
+
+    let hours = 0, minutes = 0 , seconds = 0;
+    let timerInterval = null;
+    let isRunning = false;
+
+    function updateTime(){
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+
+        timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
     }
-    else if (cleanedInput === reversed) {
-        result.innerHTML = `"${input}" is a palindrome.`;
-        result.style.color = "green";
-    } else {
-        result.innerHTML = `"${input}" is not a palindrome.`;
-        result.style.collor = "red";
-    }
-}
+
+    startBtn.addEventListener("click", ()=>{
+        if(!isRunning){
+            timerInterval = setInterval(updateTime, 1000);
+            isRunning = true;
+        }
+    });
+    
+    pauseBtn.addEventListener("click", ()=>{
+        clearInterval(timerInterval);
+        isRunning = false;
+    });
+
+    resetBtn.addEventListener("click", ()=>{
+        clearInterval(timerInterval);
+        isRunning = false;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        timeDisplay.textContent = "00:00:00";
+    });
+});
